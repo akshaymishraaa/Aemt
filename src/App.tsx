299f263a805403d86d9554ce9226025c57a78c2d
shanker9177 <gourishanker697@gmail.com>
2023-09-21 21:24:42 +0530

    registrationform.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import Dashboard from "./components/dashboard/Dashboard";
+import Registration from "./components/registration/Registration";
 
 function App() {
   return <React.Fragment>
-    Aemt App...............
-    </React.Fragment>;
+    <Registration/>
+  </React.Fragment>;
 }
 
 export default App;
