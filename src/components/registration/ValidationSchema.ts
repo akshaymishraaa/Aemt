83d9286849198a8e9c01e19595f495ffa6f3bdf5
shanker9177 <gourishanker697@gmail.com>
2023-09-22 08:01:45 +0530

    formvalidation

--- src/components/registration/ValidationSchema.ts
+++ src/components/registration/ValidationSchema.ts
@@ -1,6 +1,13 @@
 import * as Yup from 'yup'
 export const ValidateRegistration = Yup.object().shape({
     orgName: Yup.string().required("required"),
-    orgMail:Yup.string().required("required").email("invalid Email")
+    orgMail: Yup.string().required("required").email("invalid Email"),
+    orgType: Yup.string().required("required"),
+    countryName: Yup.string().required("required"),
+    stateName: Yup.string().required("required"),
+    cityName: Yup.string().required("reqired"),
+    orgPinCode: Yup.string().required("required"),
+    orgContactNumber: Yup.string().required("required").max(10, "Cant Be More then 10digits"),
+    registeredBy: Yup.string().required("required")
 
 })
