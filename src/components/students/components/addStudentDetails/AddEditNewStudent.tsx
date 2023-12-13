6f3856259492f46d2e8305ae24b940774ccbf9c9
Akshay mishra <akshaymishra1228@gmail.com>
2023-12-13 23:56:32 +0530

    changes

--- src/components/students/components/addStudentDetails/AddEditNewStudent.tsx
+++ src/components/students/components/addStudentDetails/AddEditNewStudent.tsx
@@ -16,6 +16,7 @@ import { Actiontypes } from '../../../../types/ActionTypes';
 import { toastEnabled } from '../../../../actions/Action';
 import { studentAdmissionDetails } from '../../constants/addStudent/AddStudentModal';
 import { studentAdmissionFrom } from "../../constants/addStudent/AddStudentTypes"
+import { Addnewstudent } from '../../../actions/actions';
 
 
 const AddEditNewStudentDetails = (props: any) => {
@@ -80,7 +81,9 @@ const AddEditNewStudentDetails = (props: any) => {
         let payload = admissionData
         payload[activeStep?.stepNo] = data
         setAdmissionData(payload)
+       
         console.log(payload,studentAdmissionDetails,"84....data")
+
        
     }
 
@@ -98,8 +101,11 @@ const AddEditNewStudentDetails = (props: any) => {
         let validateCompleteSubmission = formsSubmisionSteps?.filter((item: any) => item.submited !== true)
         if (validateCompleteSubmission?.length === 0) {
             let payload= studentAdmissionDetails
+            dispatch(Addnewstudent(payload,(data:any)=>{
+                console.log('sdata....',data)
+                dispatch(toastEnabled({ summary: data.message, detail: data.message, severity: 'success', show: true }))
+            }))
             console.log(Object.keys(payload)?.map((item:any)=>console.log(Object.keys(item),"67..objects")),"45")
-            dispatch(toastEnabled({ summary: 'Admission Completed', detail: 'Student Enrolled success fully', severity: 'success', show: true }))
             // navigate('/students')
 
         }
