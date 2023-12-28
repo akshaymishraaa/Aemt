eeed8ce765de59fbf98e1d974350e1d523aad05b
Amar pratap <amarprataphzb@gmail.com>
2023-12-28 19:10:35 +0530

    chnages

--- src/components/students/components/StudentDetails.tsx
+++ src/components/students/components/StudentDetails.tsx
@@ -9,7 +9,10 @@ import CommonCard from '../../../common/CommonCard';
 import CommonSearchField from '../../../common/CommonSearchField';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import '../styles/Styles.scss'
-
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 export default function StudentDetails() {
 
@@ -25,14 +28,49 @@ export default function StudentDetails() {
 
     ]
 
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 200,
+        
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+      
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     const actionBody = (rowData: any) => {
         return (
             <React.Fragment>
                 <div>
                     <DeleteIcon /> |  &nbsp;
                     <EditIcon />   |  &nbsp;
-                    <span><MoreVertIcon/></span>
+                    <span onClick={handleOpen}><MoreVertIcon /></span>
                     
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                                Text in a modal
+                            </Typography>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                            </Typography>
+                        </Box>
+                    </Modal>
+
 
                 </div>
             </React.Fragment>
@@ -70,9 +108,12 @@ export default function StudentDetails() {
                         <Column field={'parentContact'} header={'Parent contact'} />
                         <Column body={actionBody} header={'Actions'} />
                     </DataTable>
+
                 </div>
 
             </div>
         </CommonCard>
+
+
     );
 }
